158393afe117908e05874499d0bc386e28345848
JASKIRAT29 <jaskiratkr29@gmail.com>
2023-03-03 18:02:50 -0500

    added users & thoughts

---
+++ routes/api/user-routes.js
@@ -0,0 +1,23 @@
+const router = require('express').Router();
+
+const {
+    getUser,
+    getSingleUser,
+    createUser,
+    updateUser,
+    deleteUser,
+    addFriend,
+    removeFriend,
+} = require('../../controllers/userController');
+
+// Set up get all and post at /api/users
+router.route('/').get(getUser).post(createUser);
+
+// Set up get, put, and delete at /api/users/:userId
+router.route('/:id').get(getSingleUser).put(updateUser).delete(deleteUser);
+
+// Add and delete a friends
+router.route('/:id/friends/friendsId').post(addFriend).delete(removeFriend);
+
+
+module.exports = router;
