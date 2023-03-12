d401487dc844c15271d4c4e00ef7993b6ab47edb
JASKIRAT29 <jaskiratkr29@gmail.com>
2023-03-11 22:47:04 -0500

    fixed error

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-const express = require("express");
+const express = require('express');
 
-const routes = require("./routes");
-const db = require("./config/connection");
+const routes = require('./routes');
+const db = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
-db.once("open", () => {
+db.once('open', () => {
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
   });
